6056e5d758f48b9a2e07816290da7141858bd744
Nicolas Paz <nicolaspaz95@gmail.com>
2017-01-03 12:29:16 -0300

    Bug1 almost fixed

--- geoloc.js
+++ geoloc.js
@@ -2,3 +2,4 @@ windows.alert("hola");
 windows.alert("Bug1 almost fixed");
 windows.alert("Bug2 detected, beggining to work it out");
 windows.alert("Bug2 FIXED!!!");
+windows.alert("Bug1 almost fixed");
