f3cb8fe07f4e829f174934b59d5f4bf3dbb7270f
Nicolas Paz <nicolaspaz95@gmail.com>
2017-01-03 13:02:46 -0300

    Bug1 arregladogit checkout bug1 listo para llevar a master

--- geoloc.js
+++ geoloc.js
@@ -4,3 +4,5 @@ windows.alert("Bug2 detected, beggining to work it out");
 windows.alert("Bug2 FIXED!!!");
 windows.alert("Bug1 almost fixed");
 windows.alert("Continuing to fix Bug1");
+windows.alert("Continuing to fix Bug1");
+windows.alert("Bug1 FIXED!!");
